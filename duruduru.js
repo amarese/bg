80b8a3a4de0a578a55793ca116ab8ef8d6c63c10
eettrrr <52900663+eettrrr@users.noreply.github.com>
2021-10-01 20:12:27 +0900

    Update duruduru.js

--- duruduru.js
+++ duruduru.js
@@ -35,7 +35,7 @@ schedules.split('|').forEach(elemOri => {
         return
     }
     let tmpArr = []
-    tmpArr[DATE] = tmpToken[0]
+    tmpArr[DATE] = tmpToken[0].trim()
     if (tmpToken[1] == 'Y') {
         tmpArr[PUBLIC] = true
     } else {
@@ -48,25 +48,57 @@ schedules.split('|').forEach(elemOri => {
         }
 
     }
-    if (splitElemOri.length>1){
+    if (splitElemOri.length > 1) {
         tmpArr[FIRSTCONTACT] = []
-        splitElemOri[1].split(/ |\t/).forEach(elem2 =>{
+        splitElemOri[1].split(/ |\t/).forEach(elem2 => {
             tmpArr[FIRSTCONTACT].push(elem2)
         })
     }
     dataSchedules.push(tmpArr)
 })
 
-console.log("dataSchedules",dataSchedules)
+console.log("dataSchedules", dataSchedules)
+
+// 가장 많은 일정글 작성자
+let summaryHost = {} //
+    // 연속으로 몇일 참석
+let summaryDate = {}
+    // 가장 많은 사람하고 한거
+
+// 이 기간동안 첨하는 사람이 젤 많은 사람
+// 둘이 젤 많이 했다
+let summaryCouple = {}
+    // 가장 많은 모집글
+let summaryCrowdHost = {} //
+    // 남의 모집글에 제일 많이 참가
+let summaryCrowdParticipate = {} //
+    // 각자 한 횟수 카운트
+let summaryNumOfCouple = {}
+    // 일정글 안쓰고 참가
+let summaryParticipate = {} //
 
 dataSchedules.forEach(elem => {
     for (let idx = 0; idx < elem[PARTICIPANTS].length - 1; idx++) {
         let user1 = elem[PARTICIPANTS][idx].trim()
+
+
+
         if (dataCoOccuranceList[user1] == undefined) {
             dataCoOccuranceList[user1] = new Set()
             dataFirstContact[user1] = new Set()
             dataBonus[user1] = 0
+
+            summaryHost[user1] = 0
+            summaryDate[user1] = new Set()
+            summaryCrowdHost[user1] = 0
+            summaryParticipate[user1] = 0
+            summaryCrowdParticipate[user1] = 0
+            summaryNumOfCouple[user1] = {}
+
         }
+
+        ++summaryHost[user1]
+        summaryDate[user1].add(elem[DATE])
 
         for (let idx2 = idx + 1; idx2 < elem[PARTICIPANTS].length; idx2++) {
             let user2 = elem[PARTICIPANTS][idx2].trim()
@@ -74,27 +106,69 @@ dataSchedules.forEach(elem => {
                 dataCoOccuranceList[user2] = new Set()
                 dataFirstContact[user2] = new Set()
                 dataBonus[user2] = 0
+
+                summaryHost[user2] = 0
+                summaryDate[user2] = new Set()
+                summaryCrowdHost[user2] = 0
+                summaryParticipate[user2] = 0
+                summaryCrowdParticipate[user2] = 0
+                summaryNumOfCouple[user2] = {}
+
             }
+
+            if (elem[PUBLIC] == false) {
+                ++summaryParticipate[user2]
+            }
+
+            summaryDate[user2].add(elem[DATE])
+
             dataCoOccuranceList[user1].add(user2)
             dataCoOccuranceList[user2].add(user1)
+
+            if (summaryNumOfCouple[user1][user2] == undefined) {
+                summaryNumOfCouple[user1][user2] = 1
+            } else {
+                ++summaryNumOfCouple[user1][user2]
+            }
+
+            if (summaryNumOfCouple[user2][user1] == undefined) {
+                summaryNumOfCouple[user2][user1] = 1
+            } else {
+                ++summaryNumOfCouple[user2][user1]
+            }
+
+            let userTempArr = user1 + ", " + user2
+            if (user1 > user2) {
+                userTempArr = user2 + ", " + user1
+            }
+            if (summaryCouple[userTempArr] == undefined) {
+                summaryCouple[userTempArr] = 1
+            } else {
+                ++summaryCouple[userTempArr]
+            }
         }
 
     }
 
-    if (elem[FIRSTCONTACT]!=undefined){
-        elem[FIRSTCONTACT].forEach(elem2 =>{
-            let firstUserIdx = Math.floor(elem2/10)
-            let secondUserIdx = elem2%10
-            let user1 = elem[PARTICIPANTS][firstUserIdx-1].trim()
-            let user2 = elem[PARTICIPANTS][secondUserIdx-1].trim()
+    if (elem[FIRSTCONTACT] != undefined) {
+        elem[FIRSTCONTACT].forEach(elem2 => {
+            let firstUserIdx = Math.floor(elem2 / 10)
+            let secondUserIdx = elem2 % 10
+            let user1 = elem[PARTICIPANTS][firstUserIdx - 1].trim()
+            let user2 = elem[PARTICIPANTS][secondUserIdx - 1].trim()
             dataFirstContact[user1].add(user2)
-            dataFirstContact[user2].add(user1)        
+            dataFirstContact[user2].add(user1)
         })
     }
     if (elem[PUBLIC] == true) {
         dataBonus[elem[PARTICIPANTS][0].trim()] += 2
+
+        ++summaryCrowdHost[elem[PARTICIPANTS][0].trim()]
+
         for (let idx = 1; idx < elem[PARTICIPANTS].length; idx++) {
             dataBonus[elem[PARTICIPANTS][idx].trim()] += 1
+
+            ++summaryCrowdParticipate[elem[PARTICIPANTS][idx].trim()]
         }
     }
 
@@ -119,17 +193,69 @@ firstContact.split('|').forEach(elem => {
 })
 */
 
-for (const key in dataBonus) {
-    dataScores.push({ score: dataBonus[key] + dataCoOccuranceList[key].size + dataFirstContact[key].size, name: key })
+function getSequenceDate(dateSet) {
+    let dates = dateSet.keys()
+    let thisDateIt = dates.next()
+    let thisDate = Number(thisDateIt.value.split('/')[1])
+    let seqDate = 1
+    let mostSeq = 1
+    while (thisDateIt.done == false) {
+        thisDateIt = dates.next()
+        if (thisDateIt.done == true) {
+            break
+        }
+        let tempDate = Number(thisDateIt.value.split('/')[1])
+        if (tempDate == thisDate + 1) {
+
+            ++seqDate
+        } else {
+            if (seqDate > mostSeq) {
+                mostSeq = seqDate
+            }
+            seqDate = 1
+        }
+        thisDate = tempDate
+    }
+    return mostSeq
 }
+
+function getCoupleAverage(coupleMap) {
+
+    let num = 0
+    let keys = Object.keys(coupleMap)
+    for (let idx = 0; idx < keys.length; idx++) {
+        num += coupleMap[keys[idx]]
+    }
+    return num / keys.length
+}
+
+for (const key in dataBonus) {
+    dataScores.push({
+        score: dataBonus[key] + dataCoOccuranceList[key].size + dataFirstContact[key].size,
+        name: key,
+        host: summaryHost[key],
+        crowdHost: summaryCrowdHost[key],
+        crowdParticipate: summaryCrowdParticipate[key],
+        date: summaryDate[key],
+        dateSeq: getSequenceDate(summaryDate[key]),
+        participate: summaryParticipate[key],
+        couple: summaryNumOfCouple[key],
+        aveCouple: getCoupleAverage(summaryNumOfCouple[key])
+
+
+    })
+}
+
+// console.log(dataScores)
+
 
 dataScores.sort(function(a, b) {
     return a.score > b.score ? -1 : a.score < b.score ? 1 : a.name > b.name ? 1 : a.name < b.name ? -1 : 0
 })
 
-console.log("dataSchedules", dataSchedules)
-console.log("dataCoOccuranceList", dataCoOccuranceList)
-console.log("dataBonus", dataBonus)
+// console.log("dataSchedules", dataSchedules)
+// console.log("dataCoOccuranceList", dataCoOccuranceList)
+// console.log("dataBonus", dataBonus)
 console.log("dataScores", dataScores)
 
 let tlbTopBoard = document.getElementById("tlbTopBoard")
@@ -229,20 +355,20 @@ dataSchedules.forEach(elem => {
     {
         let td = document.createElement('td')
         tr.appendChild(td)
-        td.setAttribute("class","colorRed")
+        td.setAttribute("class", "colorRed")
         let tmpVal = ""
-        if (elem[FIRSTCONTACT]!=undefined){
-            console.log(elem[FIRSTCONTACT])
-            elem[FIRSTCONTACT].forEach(elem2 =>{
-                
-                let firstUserIdx = Math.floor(elem2/10)
-                let secondUserIdx = elem2%10
-                let user1 = elem[PARTICIPANTS][firstUserIdx-1].trim()
-                let user2 = elem[PARTICIPANTS][secondUserIdx-1].trim()
-                tmpVal += " (" + user1 +", "+user2+") "
+        if (elem[FIRSTCONTACT] != undefined) {
+            //console.log(elem[FIRSTCONTACT])
+            elem[FIRSTCONTACT].forEach(elem2 => {
+
+                let firstUserIdx = Math.floor(elem2 / 10)
+                let secondUserIdx = elem2 % 10
+                let user1 = elem[PARTICIPANTS][firstUserIdx - 1].trim()
+                let user2 = elem[PARTICIPANTS][secondUserIdx - 1].trim()
+                tmpVal += " (" + user1 + ", " + user2 + ") "
             })
         }
-            td.innerHTML = tmpVal
+        td.innerHTML = tmpVal
     }
 
 
