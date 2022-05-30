d4757af95aafe6215f8f17aaa72dd396fc6952c0
eettrrr <52900663+eettrrr@users.noreply.github.com>
2022-05-30 16:40:16 +0900

    Update deceptionP.js

--- deceptionP.js
+++ deceptionP.js
@@ -2,6 +2,12 @@ funcWidthPerHeight(0)
 
 funcUpdatePageSize(true)
 
+let flagFigure = true;
+
+if (flagFigure == false) {
+    document.getElementsByClassName('bgdiv')[0].style.backgroundImage = "url('')"
+}
+
 
 let cluesArr = []
 for (let idx = 0; idx < 201; idx++) {
@@ -14,7 +20,9 @@ for (let idx = 0; idx < 90; idx++) {
 }
 
 let seedNumber = 1008
-seedNumber = prompt("방번호 입력")
+if (flagFigure == true) {
+    seedNumber = prompt("방번호 입력")
+}
 let numOfPlayers = seedNumber % 100
 if (!(numOfPlayers >= 4 && numOfPlayers <= 12)) {
     numOfPlayers = 8
@@ -24,10 +32,13 @@ MMath.seedrandom(seedNumber)
 funcSortArr(cluesArr)
 funcSortArr(meansArr)
 
-checkArr = [
+let checkArr = [
     [],
     []
 ]
+
+let allCheckArr = null
+let allCheckID = []
 
 function funcBorder(idx1, idx2) {
     if (checkArr[idx1][idx2].style.border == "0px" || checkArr[idx1][idx2].style.border == "") {
@@ -47,15 +58,65 @@ function funcBorder(idx1, idx2) {
 let btnChangeP = []
 
 function funcChangePlayer(curPlayer) {
-    let curFontSize
-    for (let idx = 0; idx < numOfPlayers; idx++) {
+    if (curPlayer == numOfPlayers) {
+        for (let idx = 0; idx < 2; idx++) {
+            for (let idx2 = 0; idx2 < 4; idx2++) {
+                checkArr[idx][idx2].style.display = "none"
+            }
+        }
+        for (let idx = 0; idx < numOfPlayers; idx++) {
+            for (let idx2 = 0; idx2 < 8; idx2++) {
+                allCheckArr[idx][idx2].style.display = 'inline'
+                let clueNumber = cluesArr[4 * idx + idx2]
+                let meanNumber = meansArr[4 * idx + idx2 - 4]
+
+                if (flagFigure) {
+                    if (idx2 < 4)
+                        allCheckArr[idx][idx2].style.backgroundImage = "url('img/deception/clues" + " (" + clueNumber + ").png')"
+                    else
+                        allCheckArr[idx][idx2].style.backgroundImage = "url('img/deception/means" + " (" + meanNumber + ").png')"
+                } else {
+                    if (idx2 < 4)
+                        allCheckArr[idx][idx2].innerHTML = "clues" + " (" + clueNumber + ").png"
+                    else
+                        allCheckArr[idx][idx2].innerHTML = "means" + " (" + meanNumber + ").png"
+
+                    allCheckArr[idx][idx2].style.fontSize = "15px"
+
+                }
+            }
+            allCheckID[idx].style.display = 'inline'
+
+
+        }
+    } else {
+        for (let idx = 0; idx < 2; idx++) {
+            for (let idx2 = 0; idx2 < 4; idx2++) {
+                checkArr[idx][idx2].style.display = "inline"
+            }
+        }
+        for (let idx = 0; idx < numOfPlayers; idx++) {
+            for (let idx2 = 0; idx2 < 8; idx2++) {
+                allCheckArr[idx][idx2].style.display = 'none'
+            }
+            allCheckID[idx].style.display = 'none'
+        }
+
+    }
+    let curFontSize = 0
+    for (let idx = 0; idx <= numOfPlayers; idx++) {
         if (idx != curPlayer) {
-            curFontSize = Number(btnChangeP[idx].style.fontSize.replace("px", ""))
+            if (curFontSize < Number(btnChangeP[idx].style.fontSize.replace("px", ""))) {
+                curFontSize = Number(btnChangeP[idx].style.fontSize.replace("px", ""))
+            }
         }
         btnChangeP[idx].style.color = "black"
         btnChangeP[idx].innerHTML = idx + 1
+        if (idx == numOfPlayers) {
+            btnChangeP[idx].innerHTML = "A"
+        }
     }
-    for (let idx = 0; idx < numOfPlayers; idx++) {
+    for (let idx = 0; idx <= numOfPlayers; idx++) {
         btnChangeP[idx].style.fontSize = curFontSize + "px"
         if (idx == curPlayer) {
             btnChangeP[idx].style.fontSize = curFontSize / 2 + "px"
@@ -65,13 +126,23 @@ function funcChangePlayer(curPlayer) {
     btnChangeP[curPlayer].innerHTML = seedNumber
     for (let idx = 0; idx < 4; idx++) {
         let clueNumber = cluesArr[4 * curPlayer + idx]
-        checkArr[0][idx].style.backgroundImage = "url('img/deception/clues" + " (" + clueNumber + ").png')"
+        if (flagFigure == true) {
+            checkArr[0][idx].style.backgroundImage = "url('img/deception/clues" + " (" + clueNumber + ").png')"
+        } else {
+            checkArr[0][idx].innerHTML = "clues" + " (" + clueNumber + ").png"
+            checkArr[0][idx].style.fontSize = "15px"
+        }
         checkArr[0][idx].style.border = "0px"
             //checkArr[0][idx].innerHTML = clueNumber
             //checkArr[0][idx].style.fontSize = "20px"
 
         let meanNumber = meansArr[4 * curPlayer + idx]
-        checkArr[1][idx].style.backgroundImage = "url('img/deception/means" + " (" + meanNumber + ").png')"
+        if (flagFigure == true) {
+            checkArr[1][idx].style.backgroundImage = "url('img/deception/means" + " (" + meanNumber + ").png')"
+        } else {
+            checkArr[1][idx].innerHTML = "means" + " (" + meanNumber + ").png"
+            checkArr[1][idx].style.fontSize = "15px"
+        }
         checkArr[1][idx].style.border = "0px"
             //checkArr[1][idx].innerHTML = meanNumber
             //checkArr[1][idx].style.fontSize = "20px"
@@ -79,16 +150,60 @@ function funcChangePlayer(curPlayer) {
 
 }
 
+
+
 function funcDrawDeception() {
     funcInsertFullScreenButton(0.01, 0.01, 0.09, 0.10)
+    if (allCheckArr == null) {
+        allCheckArr = []
+        for (let idx = 0; idx < numOfPlayers; idx++) {
+            allCheckArr[idx] = []
+            allCheckID[idx] = funcInsertElement(
+                "allCheckIDBtn" + idx,
+                "button",
+                "btnTrans",
+                0.1 + idx * 0.24,
+                0.005,
+                0.1 + idx * 0.24 + 0.22,
+                0.1
+            )
+            allCheckID[idx].style.display = "none"
+            allCheckID[idx].innerHTML = (idx + 1)
+            for (let idx2 = 0; idx2 < 4; idx2++) {
+                allCheckArr[idx][idx2] = funcInsertElement(
+                    "allCheckBtn" + idx + "_" + idx2,
+                    "button",
+                    "btnTrans",
+                    0.1 + idx * 0.24,
+                    0.1 + 0.9 * (1 / 4 * idx2 + 0.005),
+                    0.1 + idx * 0.24 + 0.11 - 0.0025,
+                    0.1 + 0.9 * (1 / 4 * (idx2 + 1) - 0.005)
+                )
+                allCheckArr[idx][4 + idx2] = funcInsertElement(
+                    "allCheckBtn" + idx + "_" + (4 + idx2),
+                    "button",
+                    "btnTrans",
+                    0.1 + idx * 0.24 + 0.11 + 0.0025,
+                    0.1 + 0.9 * (1 / 4 * idx2 + 0.005),
+                    0.1 + idx * 0.24 + 0.22,
+                    0.1 + 0.9 * (1 / 4 * (idx2 + 1) - 0.005)
+                )
+                allCheckArr[idx][idx2].style.display = "none"
+            }
 
-    for (let idx = 0; idx < numOfPlayers; idx++) {
+        }
+    }
+
+    for (let idx = 0; idx <= numOfPlayers; idx++) {
         btnChangeP[idx] = funcInsertElement("changePlayerBtn_" + idx, "button", "btnTrans",
             0.01, 0.1 + idx / numOfPlayers * 0.9,
             0.09, 0.1 + (1 + idx) / numOfPlayers * 0.9
         )
         btnChangeP[idx].onclick = function() { funcChangePlayer(idx) }
         btnChangeP[idx].innerHTML = (idx + 1)
+        if (idx == numOfPlayers) {
+            btnChangeP[idx].innerHTML = "A"
+        }
     }
 
     for (let idx = 0; idx < 4; idx++) {
@@ -129,3 +244,4 @@ $(window).resize(function() {
 });
 
 funcDrawDeception()
+funcChangePlayer(numOfPlayers)
