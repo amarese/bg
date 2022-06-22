1dccfdfaf76275254d3e37984b52c5b96818ff0d
morningM00N <60580554+morningM00N@users.noreply.github.com>
2022-06-22 20:18:20 +0900

    Update jhmodule.py

--- python/jhmodule.py
+++ python/jhmodule.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import jhconstants
 class JHSelenium:
     def __init__(self,browser:webdriver.Chrome, wait:WebDriverWait) -> None:
         self.browser = browser
@@ -10,4 +10,8 @@ class JHSelenium:
     def waitAndClick(self, BY, id):
         self.wait.until(EC.element_to_be_clickable((BY,id)))
         self.browser.find_element(BY,id).click()
-
+        
+def askyesno(title, contents, defaultValue = True):
+    if jhconstants.DEBUG == True:
+        return defaultValue
+    return askyesno(title,contents)
